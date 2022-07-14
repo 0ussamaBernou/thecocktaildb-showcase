272d0a8d3e2829ad8bb79bba006a90a39d494ece
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-07-14 12:44:32 +0100

    added drinks names and null response handling

--- js/main.js
+++ js/main.js
@@ -10,14 +10,23 @@ function fetchDrink() {
     .then((res) => res.json())
     .then((data) => {
       console.log(data.drinks);
+      let drinks = data.drinks;
+      // handling null response
+      if (!drinks)
+        hero.innerHTML = `<h1>Invalid cocktail, try another one :)</h1>`;
       /* images Fetch */
       let drinksHtml = "";
+      let drinksNamesHtml = [];
+
       data.drinks.forEach((drink) => {
         drinksHtml += `<div class="drink" data-drink style="background-image: url('${drink.strDrinkThumb}');"></div>`;
+        //adding drink name to image div
+        drinksNamesHtml.push(`<h5 class='drink-name'>${drink.strDrink}</h5>`);
       });
+      console.log(drinksNamesHtml);
       hero.innerHTML = drinksHtml;
       const drinkDivs = document.querySelectorAll("[data-drink]");
-      drinkDivs.forEach((drinkDiv) => {
+      drinkDivs.forEach((drinkDiv, i) => {
         drinkDiv.addEventListener("click", () => {
           drinkDiv.classList.add("active");
           console.log("active class added");
@@ -25,9 +34,8 @@ function fetchDrink() {
             if (divDrink != drinkDiv) divDrink.classList.remove("active");
           });
         });
+        drinkDiv.innerHTML = drinksNamesHtml[i];
       });
-      /* drink name fetch */
-      
     })
     .catch((err) => {
       console.error(err);
