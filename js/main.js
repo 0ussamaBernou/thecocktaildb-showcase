3decf8c603b345a83c9f6f08b961f9b96145f75f
0ussamaBernou <oussama_bernou@um5.ac.ma>
2022-07-14 11:48:36 +0100

    images carousel working fine

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
+const container = document.querySelector("[data-container]");
+const hero = document.querySelector("[data-hero]");
+const input = document.querySelector("[data-input]");
 
 function fetchDrink() {
   hero.innerHTML = "";
   fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`
-    )
+  )
     .then((res) => res.json())
     .then((data) => {
       console.log(data.drinks);
+      /* images Fetch */
+      let drinksHtml = "";
       data.drinks.forEach((drink) => {
-        hero.innerHTML += `<div class="drink" data-drink style="background-image: url('${drink.strDrinkThumb}');"></div>`;
+        drinksHtml += `<div class="drink" data-drink style="background-image: url('${drink.strDrinkThumb}');"></div>`;
       });
+      hero.innerHTML = drinksHtml;
+      const drinkDivs = document.querySelectorAll("[data-drink]");
+      drinkDivs.forEach((drinkDiv) => {
+        drinkDiv.addEventListener("click", () => {
+          drinkDiv.classList.add("active");
+          console.log("active class added");
+          drinkDivs.forEach((divDrink) => {
+            if (divDrink != drinkDiv) divDrink.classList.remove("active");
+          });
+        });
+      });
+      /* drink name fetch */
+      
     })
     .catch((err) => {
       console.error(err);
     });
-    const drinkDivs = document.querySelectorAll("[data-drink]");
-  }
-  const button = document.querySelector("[data-btn]");
-  
-  button.addEventListener("click", fetchDrink);
-  
-  const container = document.querySelector("[data-container]");
-  const hero = document.querySelector("[data-hero]");
-  const input = document.querySelector("[data-input]");
+}
+const button = document.querySelector("[data-btn]");
 
-drinkDivs.forEach( (drinkDiv) => {
-   drinkDiv.addEventListener("click", () => {
-    drinkDiv.classList.add("active");
-    console.log("class added");
-  });
-   console.log(drinkDiv);
-});
+button.addEventListener("click", fetchDrink);
